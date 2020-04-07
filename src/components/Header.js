76dcdd2420f12e745fad502890f934e0461fa46b
cbonoz <chrisdistrict@gmail.com>
2020-04-07 09:15:42 -0400

    first commit

---
+++ src/components/Header.js
@@ -0,0 +1,66 @@
+import PropTypes from 'prop-types'
+import React from 'react'
+
+const Header = props => (
+  <header id="header" style={props.timeout ? { display: 'none' } : {}}>
+    <div className="logo">
+      <span className="icon fa-wifi"></span>
+    </div>
+    <div className="content">
+      <div className="inner">
+        <h1>Map My LTE</h1>
+        <p>
+          A consumer and industrially designed mobile app for measuring and
+          analyzing LTE data wherever you are.
+        </p>
+      </div>
+    </div>
+    <nav>
+      <ul>
+        <li>
+          <button
+            onClick={() => {
+              props.onOpenArticle('intro')
+            }}
+          >
+            Intro
+          </button>
+        </li>
+        <li>
+          <button
+            onClick={() => {
+              props.onOpenArticle('work')
+            }}
+          >
+            Work
+          </button>
+        </li>
+        <li>
+          <button
+            onClick={() => {
+              props.onOpenArticle('about')
+            }}
+          >
+            About
+          </button>
+        </li>
+        <li>
+          <button
+            onClick={() => {
+              props.onOpenArticle('contact')
+            }}
+          >
+            Contact
+          </button>
+        </li>
+      </ul>
+    </nav>
+  </header>
+)
+
+Header.propTypes = {
+  onOpenArticle: PropTypes.func,
+  timeout: PropTypes.bool,
+}
+
+export default Header
