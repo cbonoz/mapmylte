7031e25b3b217b93429de068bed9969aa86fd2d5
jihye13621 <jihye13621@yahoo.com>
2020-09-28 17:35:59 -0700

    images, content

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: 'Gatsby Starter - Dimension V2',
-    author: 'Hunter Chang',
-    description: 'A Gatsby.js V2 Starter based on Dimension by HTML5 UP',
+    title: 'Map My LTE',
+    author: 'Map My LTE',
+    description: 'Mapping LTE to Save Lives',
   },
   plugins: [
     'gatsby-plugin-react-helmet',
